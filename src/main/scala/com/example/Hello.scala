7ca255e4eebf941d0810a6120fddd51a89fc7be8
shigemk2 <i.am.shige@gmail.com>
2016-06-11 18:56:51 +0900

    List database

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,16 @@
 package com.example
 
+import com.treasuredata.client.TDClient
+import com.treasuredata.client.model.TDDatabase
+import scala.collection.JavaConversions._
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val client: TDClient = TDClient.newClient()
+    val list: java.util.List[TDDatabase] = client.listDatabases()
+    for(database <- list) println(database.getName)
+    // Never forget to close the TDClient.
+    // Program won't stop if client.close doesn't exist.
+    client.close()
   }
 }
