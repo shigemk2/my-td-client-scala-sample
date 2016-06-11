01f5840dd58edcbf8cfdf17fa8ee37efa3a9328f
shigemk2 <i.am.shige@gmail.com>
2016-06-11 19:52:27 +0900

    Job

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,7 @@
 package com.example
 
-import com.treasuredata.client.TDClient
-import com.treasuredata.client.model.{TDJobRequest, TDDatabase}
+import com.treasuredata.client.{ExponentialBackOff, TDClient}
+import com.treasuredata.client.model._
 import scala.collection.JavaConversions._
 
 object Hello {
@@ -18,6 +18,17 @@ object Hello {
     val jobId: String = client.submit(TDJobRequest.newHiveQuery("sample_db", "SELECT v['code'] AS code, COUNT(1) AS cnt FROM www_access GROUP BY v['code']"))
     println(jobId)
 
+    val backOff: ExponentialBackOff = new ExponentialBackOff()
+    var job: TDJobSummary = client.jobStatus(jobId)
+    while(!job.getStatus().isFinished()) {
+      Thread.sleep(backOff.nextWaitTimeMillis())
+      job = client.jobStatus(jobId)
+    }
+
+    val jobInfo: TDJob = client.jobInfo(jobId)
+    println(jobInfo.getCmdOut())
+    println(jobInfo.getStdErr())
+    
     // Never forget to close the TDClient.
     // Program won't stop if client.close doesn't exist.
     client.close()
