67f0e355a06d77d569ffc39e0938461ebedf77e9
shigemk2 <i.am.shige@gmail.com>
2016-06-12 20:04:40 +0900

    Print query result
    翻訳するのに
    import com.google.common.base.Function
    が必要であった

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.11.7"
 
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
+  "org.msgpack" % "msgpack-core" % "0.8.7",
+  "com.google.guava" % "guava" % "19.0",
   "com.treasuredata.client" % "td-client" % "0.7.19",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 )
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,12 @@
 package com.example
 
+import com.google.common.base.Function
+import java.io.InputStream
+import java.util.zip.GZIPInputStream
+
 import com.treasuredata.client.{ExponentialBackOff, TDClient}
 import com.treasuredata.client.model._
+import org.msgpack.core.MessagePack
 import scala.collection.JavaConversions._
 
 object Hello {
@@ -28,7 +33,22 @@ object Hello {
     val jobInfo: TDJob = client.jobInfo(jobId)
     println(jobInfo.getCmdOut())
     println(jobInfo.getStdErr())
-    
+
+    client.jobResult(jobId, TDResultFormat.MESSAGE_PACK_GZ, new Function[InputStream, Int]{
+      def apply(input: InputStream): Int = {
+        var count = 0
+        try {
+          val unpacker = MessagePack.newDefaultUnpacker(new GZIPInputStream(input))
+          while (unpacker.hasNext()) {
+            val array = unpacker.unpackValue().asArrayValue()
+            println(array)
+            count += 1
+          }
+        }
+        count
+      }
+    })
+
     // Never forget to close the TDClient.
     // Program won't stop if client.close doesn't exist.
     client.close()
